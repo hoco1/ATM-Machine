dff71d755b0863d17dcab6f092b51a7ce040f4b5
hoco1 <mohammadrezanorouzi600@gmail.com>
2023-03-30 19:46:53 +0430

    menu enum

---
+++ Enums.cs
@@ -0,0 +1,12 @@
+namespace BankAccount.Menu
+{
+    public enum Menu
+    {   
+        CheckBalance = 1,
+        Withdraw,
+        Transfer,
+        BillPayment,
+        ShowAllAccounts,
+        Exit
+    }
+}
